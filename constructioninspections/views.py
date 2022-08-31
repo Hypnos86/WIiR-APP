bfecf2cda936e0e7eb610e1d4b70abbc5f27307e
Kamil Kubiak <kamil.kubiak@po.policja.gov.pl>
2022-08-31 15:17:34 +0200

    create templates for inspections

--- constructioninspections/views.py
+++ constructioninspections/views.py
@@ -1,51 +1,74 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from constructioninspections.models import BuildingInspectionOneYear, BuildingInspectionFiveYear, ChimneyInspection, \
     ElectricalInspection, HeatingBoilerInspection, AirConditionerInspection, FireInspection
 
 
-# Create your views here.
+# Create your views here
+@login_required
 def contraction_inspection_menu(request):
-    objects = BuildingInspectionOneYear.objects.all().order_by("date_protocol"), \
-              BuildingInspectionFiveYear.objects.all().order_by("date_protocol"), \
-              ChimneyInspection.objects.all().order_by("date_protocol"), \
-              ElectricalInspection.objects.all().order_by("date_protocol"), \
-              AirConditionerInspection.objects.all().order_by("date_protocol"), \
-              FireInspection.objects.all().order_by("date_protocol")
-    return render(request, "constructioninspections/navigation.html", {"objects": objects})
+    return render(request, "constructioninspections/base_inspections.html", {})
 
 
+@login_required
+def important_inspections(request):
+    buildings_inspections_one_year = BuildingInspectionOneYear.objects.all().order_by("date_protocol")[:3]
+    buildings_inspections_five_year = BuildingInspectionFiveYear.objects.all().order_by("date_protocol")[:3]
+    chimneys_inspections = ChimneyInspection.objects.all().order_by("date_protocol")[:3]
+    electricial_inspections = ElectricalInspection.objects.all().order_by("date_protocol")[:3]
+    heating_boiler_inspections = HeatingBoilerInspection.objects.all().order_by("date_protocol")[:3]
+    air_conditioners_inspection = AirConditionerInspection.objects.all().order_by("date_protocol")[:3]
+    fire_inspections = FireInspection.objects.all().order_by("date_protocol")[:3]
+
+    context = {"buildings_inspections_one_year": buildings_inspections_one_year,
+               "buildings_inspections_five_year": buildings_inspections_five_year,
+               "chimneys_inspections": chimneys_inspections, "electricial_inspections": electricial_inspections,
+               "air_conditioners_inspection": air_conditioners_inspection,
+               "heating_boiler_inspections": heating_boiler_inspections,
+               "fire_inspections": fire_inspections}
+    return render(request, "constructioninspections/inspections.html", context)
+
+
+@login_required
 def create_buildings_inspections_choise(request):
     return render(request, "constructioninspections/buildings_inspection_all_list.html", {})
 
 
+@login_required
 def create_buildings_one_year_inspections(request):
     return render(request, "constructioninspections/buildings_one_year_inspection.html")
 
 
+@login_required
 def create_buildings_five_year_inspections(request):
     return render(request, "constructioninspections/buildings_five_year_inspection.html")
 
 
+@login_required
 def create_chimney_inspection_list(request):
     objects = ChimneyInspection.objects.all()
     return render(request, "constructioninspections/chimneys_inspection_list.html", {"objects": objects})
 
 
+@login_required
 def create_electrical_inspection_list(request):
     objects = ElectricalInspection.objects.all()
     return render(request, "constructioninspections/electrical_inspection_list.html", {"objects": objects})
 
 
+@login_required
 def create_heating_boilers_inspection_list(request):
     objects = HeatingBoilerInspection.objects.all()
     return render(request, "constructioninspections/heating_boilers_inspection_list.html", {"objects": objects})
 
 
+@login_required
 def create_air_conditioners_inspection_list(request):
     objects = AirConditionerInspection.objects.all()
     return render(request, "constructioninspections/air_conditioners_inspection_list.html", {"objects": objects})
 
 
+@login_required
 def create_fire_inspection_list(request):
     objects = FireInspection.objects.all()
     return render(request, "constructioninspections/fire_inspection_list.html", {"objects": objects})
--- investments/views.py
+++ investments/views.py
@@ -14,24 +14,24 @@ from gallery.models import Gallery
 @login_required
 def make_important_task_investments(request):
     settelments = GuaranteeSettlement.objects.all()
-    return render(request, 'investments/investments_main.html', {'settelments': settelments})
+    return render(request, "investments/investments_main.html", {"settelments": settelments})
 
 
 @login_required()
 def investment_projects_list(request):
-    projects = Project.objects.all().order_by('-date_of_acceptance')
+    projects = Project.objects.all().order_by("-date_of_acceptance")
     projects_sum = len(projects)
-    query = 'Wyczyść'
-    search = 'Szukaj'
+    query = "Wyczyść"
+    search = "Szukaj"
 
     paginator = Paginator(projects, 40)
-    page_number = request.GET.get('page')
+    page_number = request.GET.get("page")
     projects_lis = paginator.get_page(page_number)
 
-    q = request.GET.get('q')
+    q = request.GET.get("q")
 
     try:
-        last_date = Project.objects.values('change').latest('change')
+        last_date = Project.objects.values("change").latest("change")
     except Project.DoesNotExist:
         last_date = None
 
@@ -46,58 +46,58 @@ def investment_projects_list(request):
 
         projects_sum = len(projects)
 
-        return render(request, 'investments/investments_projects.html', {'projects': projects,
-                                                                         'query': query,
-                                                                         'last_date': last_date,
-                                                                         'projects_sum': projects_sum, 'q': q})
+        return render(request, "investments/investments_projects.html", {"projects": projects,
+                                                                         "query": query,
+                                                                         "last_date": last_date,
+                                                                         "projects_sum": projects_sum, "q": q})
     else:
-        return render(request, 'investments/investments_projects.html', {'projects': projects_lis,
-                                                                         'search': search,
-                                                                         'last_date': last_date,
-                                                                         'projects_sum': projects_sum})
+        return render(request, "investments/investments_projects.html", {"projects": projects_lis,
+                                                                         "search": search,
+                                                                         "last_date": last_date,
+                                                                         "projects_sum": projects_sum})
 
 
 @login_required
 def add_new_project(request):
     project_form = ProjectForm(request.POST or None, request.FILES or None)
-    project_form.fields['worker'].queryset = Employer.objects.filter(industry_specialist=True)
+    project_form.fields["worker"].queryset = Employer.objects.filter(industry_specialist=True)
     units = Unit.objects.all()
 
-    context = {'project_form': project_form,
-               'units': units,
-               'new': True
+    context = {"project_form": project_form,
+               "units": units,
+               "new": True
                }
 
-    if request.method == 'POST':
+    if request.method == "POST":
         if project_form.is_valid():
             instance = project_form.save(commit=False)
             instance.author = request.user
             project_form.save()
-            return redirect('investments:investment_projects_list')
-    return render(request, 'investments/project_form.html', context)
+            return redirect("investments:investment_projects_list")
+    return render(request, "investments/project_form.html", context)
 
 
 @login_required
 def edit_project(request, id):
     project_edit = get_object_or_404(Project, pk=id)
     project_form = ProjectForm(request.POST or None, request.FILES or None, instance=project_edit)
-    project_form.fields['worker'].queryset = Employer.objects.all().filter(industry_specialist=True)
+    project_form.fields["worker"].queryset = Employer.objects.all().filter(industry_specialist=True)
     units = Unit.objects.all()
     unit_edit = project_edit.unit
 
-    context = {'project_form': project_form,
-               'units': units,
-               'project': project_edit,
-               'unit_edit': unit_edit,
-               'new': False}
+    context = {"project_form": project_form,
+               "units": units,
+               "project": project_edit,
+               "unit_edit": unit_edit,
+               "new": False}
 
-    if request.method == 'POST':
+    if request.method == "POST":
         if project_form.is_valid():
             instance = project_form.save(commit=False)
             instance.author = request.user
             project_form.save()
-            return redirect('investments:investment_projects_list')
-    return render(request, 'investments/project_form.html', context)
+            return redirect("investments:investment_projects_list")
+    return render(request, "investments/project_form.html", context)
 
 
 @login_required
@@ -105,19 +105,19 @@ def show_project(request, id):
     project = Project.objects.get(pk=id)
     galleries = project.gallery.all()
     contracts = project.contract_auction.all()
-    context = {'project_form': project,
-               'contracts': contracts,
-               'galleries': galleries
+    context = {"project_form": project,
+               "contracts": contracts,
+               "galleries": galleries
                }
-    return render(request, 'investments/show_project.html', context)
+    return render(request, "investments/show_project.html", context)
 
 
 @login_required
 def show_galleries_popup(request, id):
     project = Project.objects.get(pk=id)
     galleries = project.gallery.all()
-    context = {'id': id,
-               'galleries': galleries}
+    context = {"id": id,
+               "galleries": galleries}
     return render(request, "investments/show_galleries_popup.html", context)
 
 
@@ -126,13 +126,13 @@ def add_contract_to_project(request, id):
     project = get_object_or_404(Project, pk=id)
     contracts = ContractAuction.objects.all().filter(investments_project=None)
 
-    if request.method == 'POST':
-        selected_contract = request.POST.get('choice')
+    if request.method == "POST":
+        selected_contract = request.POST.get("choice")
         if selected_contract:
             contract = get_object_or_404(ContractAuction, pk=selected_contract)
             contract.investments_project = project
             contract.save()
-        return redirect(reverse('investments:edit_project', kwargs={'id': id}))
+        return redirect(reverse("investments:edit_project", kwargs={"id": id}))
 
-    return render(request, 'investments/add_contract_to_project_form.html', {'contracts': contracts,
-                                                                             'project_id': id})
+    return render(request, "investments/add_contract_to_project_form.html", {"contracts": contracts,
+                                                                             "project_id": id})
