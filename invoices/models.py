830272a8223ad046958b3714a3d49daed55eb56e
Kamil Kubiak <kamil.kubiak@po.policja.gov.pl>
2022-11-17 13:05:18 +0100

    tworzenie weryfikacji i dodatnie do modelu invoices relatedName

--- invoices/models.py
+++ invoices/models.py
@@ -23,23 +23,25 @@ class InvoiceSell(models.Model):
         verbose_name_plural = "Faktury - sprzedaż"
         ordering = ["-date"]
 
+    relatedName = "invoicesell"
+
     date = models.DateField("Data wystawienia")
     no_invoice = models.CharField("Nr. faktury", max_length=11)
     doc_types = models.ForeignKey(DocumentTypes, null=False, blank=False, on_delete=models.CASCADE,
-                                  verbose_name="Rodzaj dokumentu", related_name="invoicesell")
+                                  verbose_name="Rodzaj dokumentu", related_name=relatedName)
     contractor = models.ForeignKey(Contractor, on_delete=models.CASCADE, verbose_name="Kontrahent",
-                                   related_name="invoicesell")
+                                   related_name=relatedName)
     sum = models.DecimalField("Kwota [zł]", max_digits=10, decimal_places=2, null=True, blank=True)
-    county = models.ForeignKey(County, on_delete=models.CASCADE, verbose_name="Powiat", related_name="invoicesell")
+    county = models.ForeignKey(County, on_delete=models.CASCADE, verbose_name="Powiat", related_name=relatedName)
     date_of_payment = models.DateField("Termin płatności")
     period_from = models.DateField("Okres od")
     period_to = models.DateField("Okres do")
     creator = models.ForeignKey(Employer, on_delete=models.CASCADE, verbose_name="Osoba wystawiająca",
-                                related_name="invoicesell")
+                                related_name=relatedName)
     information = models.TextField("Informacje", blank=True, default="")
     creation_date = models.DateTimeField("Data utworzenia", auto_now_add=True)
     change_date = models.DateTimeField("Zmiana", auto_now=True)
-    author = models.ForeignKey("auth.User", on_delete=models.CASCADE, related_name="invoicesell")
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE, related_name=relatedName)
 
     def __str__(self):
         return f"Faktura nr {self.no_invoice} z dnia {self.date} na kwotę {self.sum} zł."
@@ -51,20 +53,22 @@ class InvoiceBuy(models.Model):
         verbose_name_plural = "Faktury - kupno"
         ordering = ["-date_receipt"]
 
+    relatedName = "invoicebuy"
+
     date_receipt = models.DateField("Data wpływu")
     date_issue = models.DateField("Data wystawienia")
     no_invoice = models.CharField("Nr. faktury", max_length=30)
     doc_types = models.ForeignKey(DocumentTypes, null=False, blank=False, on_delete=models.CASCADE,
                                   verbose_name="Rodzaj dokumentu",
-                                  related_name="invoicebuy")
+                                  related_name=relatedName)
     sum = models.DecimalField("Kwota [zł]", max_digits=10, decimal_places=2, null=True, blank=True)
     date_of_payment = models.DateField("Termin płatności")
     contractor = models.ForeignKey(Contractor, on_delete=models.CASCADE, verbose_name="Kontrahent",
-                                   related_name="invoicebuy")
+                                   related_name=relatedName)
     information = models.TextField("Informacje", blank=True, default="")
     creation_date = models.DateTimeField("Data utworzenia", auto_now_add=True)
     change_date = models.DateTimeField("Zmiana", auto_now=True)
-    author = models.ForeignKey("auth.User", on_delete=models.CASCADE, related_name="invoicebuy")
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE, related_name=relatedName)
 
     def __str__(self):
 
@@ -77,12 +81,14 @@ class InvoiceItems(models.Model):
         verbose_name_plural = "Elementy faktury"
         ordering = ["invoice_id"]
 
+    relatedName = "invoice_items"
+
     invoice_id = models.ForeignKey(InvoiceBuy, on_delete=models.CASCADE, verbose_name="Faktura",
-                                   related_name="invoice_items")
+                                   related_name=relatedName)
     account = models.ForeignKey(FinanceSource, on_delete=models.CASCADE, verbose_name="Konto",
-                                related_name="invoice_items")
+                                related_name=relatedName)
     county = models.ForeignKey(County, on_delete=models.CASCADE, verbose_name="Powiat",
-                               related_name="invoice_items")
+                               related_name=relatedName)
     sum = models.DecimalField("Kwota brutto [zł]", max_digits=10, decimal_places=2, null=True, blank=True)
 
     def __str__(self):
@@ -95,15 +101,17 @@ class CorrectiveNote(models.Model):
         verbose_name_plural = "Noty korygujące"
         ordering = ["-date"]
 
+    relatedName = "correctivenote"
+
     date = models.DateField("Data wystawienia")
     no_note = models.CharField("Nr. noty", max_length=15)
     contractor = models.ForeignKey(Contractor, on_delete=models.CASCADE, verbose_name="Kontrahent",
-                                   related_name="correctivenote")
+                                   related_name=relatedName)
     corrective_invoice = models.CharField("Korygowana faktura", max_length=70)
     information = models.TextField("Korygowana treść", blank=True, default="")
     creation_date = models.DateField("Data uworzenia", auto_now_add=True)
     author = models.ForeignKey("auth.User", on_delete=models.CASCADE, verbose_name="Autor",
-                               related_name="correctivenote")
+                               related_name=relatedName)
 
     def __str__(self):
         return f"{self.no_note} z dnia {self.date}"
