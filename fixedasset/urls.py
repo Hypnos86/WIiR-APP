0dce7c626fdb25e1d04bddcbaf53450c1854e759
Kamil Kubiak <kamil.kubiak@po.policja.gov.pl>
2023-02-27 15:16:17 +0100

    poprawienie url

--- fixedasset/urls.py
+++ fixedasset/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
 from fixedasset.views import fixed_asset_list, show_information, add_new_building, edit_building
 
 app_name = "fixedasset"
 
 urlpatterns = [
     path("recordsBuildings/", fixed_asset_list, name="fixed_asset_list"),
-    path("infoPopup/<int:id>/", show_information, name="show_information"),
+    re_path("infoPopup/(?P<id>\d+)/$", show_information, name="show_information"),
     path("add_Building/", add_new_building, name="add_new_building"),
-    path("editBuilding/<int:id>/", edit_building, name="edit_building")
+    re_path("editBuilding/(?P<id>\d+)/$", edit_building, name="edit_building")
 ]
--- investments/urls.py
+++ investments/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from investments.views import make_important_task_investments, investment_projects_list, add_new_project, edit_project, \
     show_project, show_galleries_popup, add_contract_to_project
 
@@ -6,10 +6,10 @@ app_name = "investments"
 urlpatterns = [
     path("investments/", investment_projects_list, name="investment_projects_list"),
     path("newInvestments/", add_new_project, name="add_new_project"),
-    path("editInvestment/<int:id>/", edit_project, name="edit_project"),
-    path("showInvestment/<int:id>/", show_project, name="show_project"),
-    path("projectGallery/<int:id>/", show_galleries_popup, name="show_galleries_popup"),
-    path("addContractToProject/<int:id>/", add_contract_to_project, name="add_contract_to_project"),
+    re_path("editInvestment/(?P<id>\d+)/$", edit_project, name="edit_project"),
+    re_path("showInvestment/(?P<id>\d+)/$", show_project, name="show_project"),
+    re_path("projectGallery/(?P<id>\d+)/$", show_galleries_popup, name="show_galleries_popup"),
+    re_path("addContractToProject/(?P<id>\d+)/$", add_contract_to_project, name="add_contract_to_project"),
     path("", make_important_task_investments, name="make_important_task_investments")
 
 ]
