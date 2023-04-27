956a445dc4a92bfb777fb01f02101cc63e5e06c4
Kamil <kubiak.kamil23@gmail.com>
2023-04-27 21:28:38 +0200

    dodanie staticow

--- WIiR_APP/settings.py
+++ WIiR_APP/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -152,6 +152,8 @@ STATICFILES_DIRS = [BASE_DIR / 'main/static']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'main:welcome'
