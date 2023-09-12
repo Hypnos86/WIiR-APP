7ea26a73cea2c3c72084a731ecb8ee0fa58447fb
Kamil Kubiak <kamil.kubiak@po.policja.gov.pl>
2023-09-12 08:10:20 +0200

    fix urls in invoices module

--- invoices/urls.py
+++ invoices/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, re_path
-from .views import menu_invoices, buy_invoices_list, buy_invoices_list_archive, new_invoice_buy, sell_invoices_list,\
-    sell_invoices_list_archive, new_invoice_sell, edit_invoice_sell, edit_invoice_sell_archive,edit_invoice_buy, make_verification, show_info_buy, \
+from .views import menu_invoices, buy_invoices_list, buy_invoices_list_archive, NewInvoiceBuyView, sell_invoices_list, \
+    sell_invoices_list_archive, new_invoice_sell, edit_invoice_sell, edit_invoice_sell_archive, edit_invoice_buy, \
+    make_verification, show_info_buy, \
     show_info_sell, corrective_note_list, corrective_note_list_archive, show_info_note, new_note, \
-    edit_note, EditNoteArchiveView, make_pdf_from_invoices_sell, AddItemsInvoiceBuyView, delete_items_invoice_buy, delete_invoice_buy
+    edit_note, EditNoteArchiveView, make_pdf_from_invoices_sell, AddItemsInvoiceBuyView, delete_items_invoice_buy, \
+    delete_invoice_buy
 
 app_name = "invoices"
 urlpatterns = [
@@ -11,7 +13,7 @@ urlpatterns = [
     path("buyList/", buy_invoices_list, name="buy_invoices_list"),
     re_path("infoBuy/(?P<id>\d+)/$", show_info_buy, name="show_info_buy"),
     re_path("archiveBuy/(?P<year>[0-9]{4})/$", buy_invoices_list_archive, name="buy_invoices_list_archive"),
-    path("newInvoice_buy/", new_invoice_buy, name="new_invoice_buy"),
+    path("newInvoice_buy/", NewInvoiceBuyView.as_view(), name="new_invoice_buy"),
     re_path("editInvoice_buy/(?P<id>\d+)/$", edit_invoice_buy, name="edit_invoice_buy"),
     re_path("deleteInvoice_by/(?P<id>\d+)/$", delete_invoice_buy, name="delete_invoice_buy"),
     re_path("addInvoiceItem/(?P<id>\d+)/$", AddItemsInvoiceBuyView.as_view(), name="add_items_invoice_buy"),
@@ -27,7 +29,8 @@ urlpatterns = [
     # Noty księgowe
     path("recordAccountingNote/", corrective_note_list, name="corrective_note_list"),
     re_path("infoNote/(?P<id>\d+)/$", show_info_note, name="show_info_note"),
-    re_path("recordAccountingNoteArchive/(?P<year>[0-9]{4})/$", corrective_note_list_archive, name="corrective_note_list_archive"),
+    re_path("recordAccountingNoteArchive/(?P<year>[0-9]{4})/$", corrective_note_list_archive,
+            name="corrective_note_list_archive"),
     path("newAccountingNote/", new_note, name="new_note"),
     re_path("editAccountingNote/(?P<id>\d+)/$", edit_note, name="edit_note"),
     re_path("editAccountingNoteArchive(?P<id>\d+)/$", EditNoteArchiveView.as_view(), name="edit_note_archive"),
