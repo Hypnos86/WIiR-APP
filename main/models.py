a5c84cc735549daa45666e1d1f8aea71c3a08ec7
Kamil Kubiak <kamil.kubiak@po.policja.gov.pl>
2023-09-14 09:50:29 +0200

    add changes in main

--- main/models.py
+++ main/models.py
@@ -72,23 +72,23 @@ class Employer(models.Model):
 
     related_name = "employer"
 
-    id_swop = models.CharField("ID SWOP", max_length=6, unique=True)
-    name = models.CharField("Imię", max_length=20)
-    last_name = models.CharField("Nazwisko", max_length=25)
-    position = models.CharField("Stanowisko", max_length=20, blank=True, default="")
+    id_swop = models.CharField(verbose_name="ID SWOP", max_length=6, unique=True)
+    name = models.CharField(verbose_name="Imię", max_length=20)
+    last_name = models.CharField(verbose_name="Nazwisko", max_length=25)
+    position = models.CharField(verbose_name="Stanowisko", max_length=20, blank=True, default="")
     team = models.ForeignKey(Team, on_delete=models.CASCADE, verbose_name="Zespół", related_name=related_name)
     industry_specialist = models.BooleanField(default=False, verbose_name="Branżysta")
     invoices_issues = models.BooleanField(default=False, verbose_name="Wystawianie faktur")
     industry = models.ForeignKey(IndustryType, on_delete=models.CASCADE, null=True, blank=True, verbose_name="Branża",
                                  related_name=related_name)
-    no_room = models.CharField("Nr. pokoju", max_length=2, blank=True, default="")
-    no_tel_room = models.CharField("Nr. telefonu", max_length=6, blank=True, default="")
-    no_tel_private = models.CharField("Nr. komórkowy", max_length=9, blank=True, default="")
-    information = models.TextField("Informacje", max_length=200, null=True, blank=True)
-    deleted = models.BooleanField("Usunięty", default=False)
+    no_room = models.CharField(verbose_name="Nr. pokoju", max_length=2, blank=True, default="")
+    no_tel_room = models.CharField(verbose_name="Nr. telefonu", max_length=6, blank=True, default="")
+    no_tel_private = models.CharField(verbose_name="Nr. komórkowy", max_length=9, blank=True, default="")
+    information = models.TextField(verbose_name="Informacje", max_length=200, null=True, blank=True)
+    deleted = models.BooleanField(verbose_name="Usunięty", default=False)
     creation_date = models.DateTimeField(auto_now_add=True)
-    change = models.DateTimeField("Data zmian", auto_now=True)
-    author = models.ForeignKey("auth.User", on_delete=models.CASCADE, related_name="employer")
+    change = models.DateTimeField(verbose_name="Data zmian", auto_now=True)
+    author = models.ForeignKey(to="auth.User", on_delete=models.CASCADE, related_name="employer")
 
     def __str__(self):
         return f"{self.name} {self.last_name}"
@@ -103,43 +103,43 @@ class AccessModule(models.Model):
 
     user = models.OneToOneField("auth.User", on_delete=models.CASCADE, verbose_name="Użytkownik",
                                 related_name=relatedName)
-    zok_team = models.BooleanField("Ewidencja: Zespoł Obsługi Kancelarynej", default=False)
-    teams = models.BooleanField("ZOK - Zespoły", default=False)
-    commands = models.BooleanField("ZOK - Polecenia", default=False)
-    employers = models.BooleanField("ZOK - Pracownicy", default=False)
-    cars = models.BooleanField("ZOK - Rezerwacja samochodu służbowego", default=False)
-    zriwt_team = models.BooleanField("Ewidencja: Zespół Rozliczeń i Wsparcia Technicznego", default=False)
-    contractors_module = models.BooleanField("ZRiWT - Kontrahenci - Podgląd", default=False)
-    contractors_module_edit = models.BooleanField("ZRiWT - Kontrahenci - Edycja", default=False)
-    invoices_module = models.BooleanField("ZRiWT - Faktury - Podgląd", default=False)
-    invoices_module_edit = models.BooleanField("ZRiWT - Faktury - Edycja", default=False)
-    donations_module = models.BooleanField("ZRiWT - Darowizny - Podgląd", default=False)
-    donations_module_edit = models.BooleanField("ZRiWT - Darowizny - Edycja", default=False)
-    buildings_module = models.BooleanField("ZRiWT - Środki trwałe - budynki - Podgląd", default=False)
-    buildings_module_edit = models.BooleanField("ZRiWT - Środki trwałe - budynki - Edycja", default=False)
-    inspections_module = models.BooleanField("ZRiWT - Przeglądy - Podgląd", default=False)
-    inspections_module_edit = models.BooleanField("ZRiWT - Przeglądy - Edycja", default=False)
-    contracts_auction_module = models.BooleanField("ZRiWT - Umowy ZZP - Podgląd", default=False)
-    contracts_auction_module_edit = models.BooleanField("ZRiWT - Umowy ZZP - Edycja", default=False)
-    investments_module = models.BooleanField("ZRiWT - Inwestycje - Podgląd", default=False)
-    investments_module_edit = models.BooleanField("ZRiWT - Inwestycje - Edycja", default=False)
-    cpvdict_module = models.BooleanField("ZRiWT - Rodzajowość - Podgląd", default=False)
-    cpvdict_module_edit = models.BooleanField("ZRiWT - Rodzajowość - Edycja", default=False)
-    gallery_module = models.BooleanField("ZRiWT - Galeria - Podgląd", default=False)
-    gallery_module_edit = models.BooleanField("ZRiWT - Galeria - Edycja", default=False)
-    zm_team = models.BooleanField("Ewidencja: Zespół Mieszkaniowy", default=False)
-    official_flat = models.BooleanField("ZM - Mieszkania służbowe - Podgląd", default=False)
-    official_flat_edit = models.BooleanField("ZM - Mieszkania służbowe - Edycja", default=False)
-    zn_team = models.BooleanField("Ewidencja: Zespół Nieruchomości", default=False)
-    units = models.BooleanField("ZN - Jednostki - Podgląd", default=False)
-    units_edit = models.BooleanField("ZN - Jednostki - Edycja", default=False)
-    contract_immovables = models.BooleanField("ZN - Umowy nieruchomości - Podgląd", default=False)
-    contract_immovables_edit = models.BooleanField("ZN - Umowy nieruchomości - Edycja", default=False)
-    ze_team = models.BooleanField("Ewidencja: Zespół Eksploatacji", default=False)
-    records_letters = models.BooleanField("ZE - Ewidencja Pism - Podgląd", default=False)
-    records_letters_edit = models.BooleanField("ZE - Ewidencja Pism - Edycja", default=False)
-    contract_media = models.BooleanField("ZE - Umowy Media - Podgląd", default=False)
-    contract_media_edit = models.BooleanField("ZE - Umowy Media - Edycja", default=False)
+    zok_team = models.BooleanField(verbose_name="Ewidencja: Zespoł Obsługi Kancelarynej", default=False)
+    teams = models.BooleanField(verbose_name="ZOK - Zespoły", default=False)
+    commands = models.BooleanField(verbose_name="ZOK - Polecenia", default=False)
+    employers = models.BooleanField(verbose_name="ZOK - Pracownicy", default=False)
+    cars = models.BooleanField(verbose_name="ZOK - Rezerwacja samochodu służbowego", default=False)
+    zriwt_team = models.BooleanField(verbose_name="Ewidencja: Zespół Rozliczeń i Wsparcia Technicznego", default=False)
+    contractors_module = models.BooleanField(verbose_name="ZRiWT - Kontrahenci - Podgląd", default=False)
+    contractors_module_edit = models.BooleanField(verbose_name="ZRiWT - Kontrahenci - Edycja", default=False)
+    invoices_module = models.BooleanField(verbose_name="ZRiWT - Faktury - Podgląd", default=False)
+    invoices_module_edit = models.BooleanField(verbose_name="ZRiWT - Faktury - Edycja", default=False)
+    donations_module = models.BooleanField(verbose_name="ZRiWT - Darowizny - Podgląd", default=False)
+    donations_module_edit = models.BooleanField(verbose_name="ZRiWT - Darowizny - Edycja", default=False)
+    buildings_module = models.BooleanField(verbose_name="ZRiWT - Środki trwałe - budynki - Podgląd", default=False)
+    buildings_module_edit = models.BooleanField(verbose_name="ZRiWT - Środki trwałe - budynki - Edycja", default=False)
+    inspections_module = models.BooleanField(verbose_name="ZRiWT - Przeglądy - Podgląd", default=False)
+    inspections_module_edit = models.BooleanField(verbose_name="ZRiWT - Przeglądy - Edycja", default=False)
+    contracts_auction_module = models.BooleanField(verbose_name="ZRiWT - Umowy ZZP - Podgląd", default=False)
+    contracts_auction_module_edit = models.BooleanField(verbose_name="ZRiWT - Umowy ZZP - Edycja", default=False)
+    investments_module = models.BooleanField(verbose_name="ZRiWT - Inwestycje - Podgląd", default=False)
+    investments_module_edit = models.BooleanField(verbose_name="ZRiWT - Inwestycje - Edycja", default=False)
+    cpvdict_module = models.BooleanField(verbose_name="ZRiWT - Rodzajowość - Podgląd", default=False)
+    cpvdict_module_edit = models.BooleanField(verbose_name="ZRiWT - Rodzajowość - Edycja", default=False)
+    gallery_module = models.BooleanField(verbose_name="ZRiWT - Galeria - Podgląd", default=False)
+    gallery_module_edit = models.BooleanField(verbose_name="ZRiWT - Galeria - Edycja", default=False)
+    zm_team = models.BooleanField(verbose_name="Ewidencja: Zespół Mieszkaniowy", default=False)
+    official_flat = models.BooleanField(verbose_name="ZM - Mieszkania służbowe - Podgląd", default=False)
+    official_flat_edit = models.BooleanField(verbose_name="ZM - Mieszkania służbowe - Edycja", default=False)
+    zn_team = models.BooleanField(verbose_name="Ewidencja: Zespół Nieruchomości", default=False)
+    units = models.BooleanField(verbose_name="ZN - Jednostki - Podgląd", default=False)
+    units_edit = models.BooleanField(verbose_name="ZN - Jednostki - Edycja", default=False)
+    contract_immovables = models.BooleanField(verbose_name="ZN - Umowy nieruchomości - Podgląd", default=False)
+    contract_immovables_edit = models.BooleanField(verbose_name="ZN - Umowy nieruchomości - Edycja", default=False)
+    ze_team = models.BooleanField(verbose_name="Ewidencja: Zespół Eksploatacji", default=False)
+    records_letters = models.BooleanField(verbose_name="ZE - Ewidencja Pism - Podgląd", default=False)
+    records_letters_edit = models.BooleanField(verbose_name="ZE - Ewidencja Pism - Edycja", default=False)
+    contract_media = models.BooleanField(verbose_name="ZE - Umowy Media - Podgląd", default=False)
+    contract_media_edit = models.BooleanField(verbose_name="ZE - Umowy Media - Edycja", default=False)
 
     def __str__(self):
         return f"{self.user} - {self.user.first_name} {self.user.last_name}"
@@ -161,8 +161,8 @@ class Command(models.Model):
     title = models.CharField("Nazwa", max_length=120)
     content = models.TextField("Treść")
     scan = models.FileField(upload_to=upload_scan, verbose_name="Skan polecenia")
-    change = models.DateTimeField("Data zmian", auto_now=True)
-    create_date = models.DateTimeField("Data dodania", auto_now_add=True)
+    change = models.DateTimeField(verbose_name="Data zmian", auto_now=True)
+    create_date = models.DateTimeField(verbose_name="Data dodania", auto_now_add=True)
 
     def __str__(self):
         return f"{self.title}"
@@ -176,15 +176,15 @@ class Car(models.Model):
 
     related_name = "car"
 
-    rent_date = models.DateField("Data", unique=True)
+    rent_date = models.DateField(verbose_name="Data", unique=True)
     borrower = models.ManyToManyField(Employer, verbose_name="Delegat", related_name=related_name)
     target = models.TextField("Cel")
-    create_date = models.DateTimeField("Data utworzenia", auto_now_add=True)
-    change = models.DateTimeField("Zmiana", auto_now=True)
-    author = models.ForeignKey("auth.User", on_delete=models.CASCADE, related_name=related_name, verbose_name="Autor")
+    create_date = models.DateTimeField(verbose_name="Data utworzenia", auto_now_add=True)
+    change = models.DateTimeField(verbose_name="Zmiana", auto_now=True)
+    author = models.ForeignKey(to="auth.User", on_delete=models.CASCADE, related_name=related_name, verbose_name="Autor")
 
     def __str__(self):
-        return f"{self.rent_date} {self.target}"
+        return f"{self.rent_date.strftime('%d.%m.%Y')} - {self.target}"
 
 
 def upload_file(instance, filename):
@@ -204,7 +204,7 @@ class NecesseryFile(models.Model):
 
     title = models.CharField(verbose_name='Nazwa pliku', max_length=300)
     file = models.FileField(upload_to=upload_file, null=False, verbose_name="Plik")
-    create_date = models.DateTimeField("Data utworzenia", auto_now_add=True)
+    create_date = models.DateTimeField(verbose_name="Data utworzenia", auto_now_add=True)
 
     def __str__(self):
         return f'{self.title}'
