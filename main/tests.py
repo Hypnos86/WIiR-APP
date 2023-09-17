b68ff8f54565f01febf1f5b90347f50b87d1a50d
Kamil <kubiak.kamil23@gmail.com>
2023-09-17 17:11:47 +0200

    create tests

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.test import Client
+from .models import Car  # Zaimportuj modele, które są używane w Twoim kodzie
 
-# Create your tests here.
+
+class TestMainViews(TestCase):
+
+    def setUp(self):
+        # Tworzenie użytkownika do testów
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+
+    def test_welcome_view(self):
+        self.client.login(username='testuser', password='testpass')
+        response = self.client.get(reverse('main:welcome'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'main/home.html')
